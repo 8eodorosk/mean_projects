8c41f0940df29249a9ce035880841f9e4529695e
Thodoris Koutsis <8eodorosk@gmail.com>
2018-10-14 21:53:57 +0300

    mac upload

--- mean_project_v2/backend/routes/posts.js
+++ mean_project_v2/backend/routes/posts.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const Post = require('../models/post');
 
 
-router.post("", (req, res, next) =>{
+const MIME_TYPE_MAP = {
+	'image/png': 'png',
+	'image/jpeg': 'jpg',
+	'image/jpg': 'jpg'
+}
+
+
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		const isValid = MIME_TYPE_MAP[file.mimetype];
+		let error = new Error("Invalid mime type");
+		if (isValid) {
+			error = null;
+		}
+		cb(error, "images");
+	},
+	filename: (req, file, cb) => {
+		const name = file.originalname.toLowerCase().split(' ').join('-');
+		const ext = MIME_TYPE_MAP[file.mimetype]; 
+		cb(null, name + '-' + Date.now() + '.' + ext);
+	}
+})
+
+// sto express mporoyme na baloyme osa arguments theloume kai tha ginontai
+// execute apo aristera sta dexia, edw px to prwto einai to path
+// kai twra tha valoyme ena middleware na trexei prin to requet
+// tha trexoume ton multer kai tha perimenei ena single file typoy 
+// image
+router.post("", multer({storage: storage}).single("image") ,(req, res, next) =>{
 	//const post = req.body;
 	
 	const post = new Post({
---
+++ mean_project_v2/frontend/src/app/posts/post-create/mime-type.validator.ts
@@ -0,0 +1,46 @@
+ import { AbstractControl } from '@angular/forms';
+ import { Observable, Observer } from 'rxjs';
+
+
+//asyncronous  validators returna a promise or a  observable
+//the promise will return a property which can be interpreted as a string and i dont care about the name and the value any
+// this is a key: value return type of a "string i dont care" : "of any value"
+// if validation passed it will return null, if not it will return an object 
+export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}>  => {
+	const file = control.value as File;
+	const fileReader = new FileReader();
+	const frObs = Observable.create((observer: Observer<{[key: string]: any}>) => {
+		fileReader.addEventListener("loadend", ()=>{
+			const arr = new Uint8Array(fileReader.result).subarray(0,4);
+			let header = "";
+			let isValid = false;
+			for(let i =0; i<arr.length; i++){
+				header +=arr[i].toString(16); //convrt this header hexadecimal string
+			}
+
+			switch (header) {
+				case "89504e47":
+					isValid = true;
+					break;
+				case "ffd8ffe0":
+				case "ffd8ffe1":
+				case "ffd8ffe2":
+				case "ffd8ffe3":
+				case "ffd8ffe8":
+					isValid = true;
+					break;
+				default:
+					isValid = false; // Or you can use the blob.type as fallback
+					break;
+        	}
+        	if (isValid) {
+        		observer.next(null);
+        	} else {
+        		observer.next({invalidMimeType: true});
+        	}
+        	observer.complete();
+		});
+		fileReader.readAsArrayBuffer(file);
+	});
+	return frObs;
+};
